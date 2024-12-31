cc03d7825c34cd2a489f1808f7c57d28fc76a6c5
Leonel Gonzalez <leonel.gonzalez@flexxus.com.ar>
2024-12-30 22:30:31 -0300

    version 0.0.1

--- src/components/ProductTitle.tsx
+++ src/components/ProductTitle.tsx
@@ -5,22 +5,19 @@ import styles from '../styles/styles.module.css'
 
 
 export interface Props {
-    className?: string 
-    title?: string, 
+    className?: string
+    title?: string,
     activeClass?: string;
-    style?: React.CSSProperties 
+    style?: React.CSSProperties
 }
 
 export const ProductTitle = ({ title, className, style }: Props) => {
 
-    const { product } = useContext( ProductContext )
+    const { product } = useContext(ProductContext)
 
     return (
-        <span 
-            className={ `${ styles.productDescription } ${ className }` }
-            style={ style }
-        >
-            { title ? title : product.title }
+        <span className={`${styles.productDescription} ${className}`} style={style}>
+            {title ? title : product.title}
         </span>
     );
 }
